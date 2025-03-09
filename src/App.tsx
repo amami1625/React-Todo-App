b447475ef023e644275d2db01f278a2a9cea8575
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-09 22:20:13 +0900

    App へステートを変更する処理を追加し、 TodoItemへ渡す

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
-import { useState } from "react"
-import TodoList from "./components/TodoList"
+import { useState } from "react";
+import TodoList from "./components/TodoList";
 import { Todo } from "./types/type";
 import TodoItem from "./components/TodoItem";
 
 function App() {
   const tempTodos = [
-    {id: 1, title: "sample1", isCompleted: false},
-    {id: 2, title: "sample2", isCompleted: false},
-    {id: 3, title: "sample3", isCompleted: false},
-  ]
+    { id: 1, title: "sample1", isCompleted: false },
+    { id: 2, title: "sample2", isCompleted: false },
+    { id: 3, title: "sample3", isCompleted: false },
+  ];
   const [todos, setTodos] = useState<Todo[]>(tempTodos);
   const [completedTodos, setCompletedTodos] = useState<string[]>([]);
 
   const handleTodoDelete = (id: number) => {
-    setTodos(todos.filter(todo => todo.id !== id));
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleTodoComplete = (id: number) => {
+    const completeTodo = todos[todos.findIndex((todo) => todo.id === id)];
+    setTodos(todos.filter((todo) => todo.id !== id));
+    setCompletedTodos([...completedTodos, completeTodo.title]);
   };
 
   return (
     <>
       <TodoList>
-        {todos.map(todo => <TodoItem todo={todo} onDelete={handleTodoDelete} key={todo.id}/>)}
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            onDelete={handleTodoDelete}
+            onComplete={handleTodoComplete}
+            key={todo.id}
+          />
+        ))}
       </TodoList>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
