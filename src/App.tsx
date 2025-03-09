a84118a7d9a1246d58fae30e0810e2d7a2dd71b8
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-09 21:36:43 +0900

    App 内にステートと仮のデータを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react"
 import TodoList from "./components/TodoList"
 
 function App() {
+  const tempTodos = [
+    {id: 1, title: "sample1", isCompleted: false},
+    {id: 2, title: "sample2", isCompleted: false},
+    {id: 3, title: "sample3", isCompleted: false},
+  ]
+  const [todos, setTodos] = useState(tempTodos);
+
   return (
     <>
       <TodoList />
