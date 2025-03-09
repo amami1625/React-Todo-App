6d70c92846c1d0a0ae04f26613080a971935bf55
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-09 21:49:47 +0900

    TodoItem へプロップスとその型を追加、スタイルの変更

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,12 +1,23 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { Todo } from "../types/type";
 
-const Li = styled.li`
-`
+const Li = styled.li``;
+const Input = styled.input``;
+const Label = styled.label``;
 
-const TodoItem = () => {
+type TodoItemProps = {
+  todo: Todo;
+};
+
+const TodoItem = ({ todo }: TodoItemProps) => {
   return (
-    <Li>Todo</Li>
-  )
-}
+    <Li>
+      <Label>
+        <Input type="checkbox" />
+        {todo.title}
+      </Label>
+    </Li>
+  );
+};
 
-export default TodoItem
+export default TodoItem;
