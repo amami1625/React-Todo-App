9f3717f35af3becaff232039784bf8f1d73772e5
amami1625 <amami_gt0403@yahoo.co.jp>
2025-03-09 21:29:42 +0900

    TodoList のスタイルを変更

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import styled from 'styled-components'
+
+const Ul = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
+    <Ul></Ul>
   )
 }
 
